df0de7af3b71f21c94c0c7073e8624b51bfd0237
rslassman <rslassman@gmail.com>
2021-10-20 20:26:09 -0400

    get lab4 content

--- Lab 3/speech2text_all_steps.sh
+++ Lab 3/speech2text_all_steps.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 say() { local IFS=+;/usr/bin/mplayer -ao alsa -really-quiet -noconsolecontrols "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$*&tl=en"; }
 #say $*
-say "What are the steps of the recipie."
+say "What are the steps of the recipe."
 
 arecord -D hw:2,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
 python3 recipie_words.py recorded_mono.wav
